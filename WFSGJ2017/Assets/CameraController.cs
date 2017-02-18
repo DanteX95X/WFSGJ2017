5381f71c871b93794ed00d5127dfdbefe606cc66
DevRaptor <krzysztoftaperek@gmail.com>
2017-02-18 05:23:32 +0100

    Added following camera

---
+++ WFSGJ2017/Assets/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	#region variables
+	GameObject player;
+	Vector3 offset;
+
+	#endregion
+
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		offset = transform.position - player.transform.position;
+	}
+	
+
+	void LateUpdate()
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
