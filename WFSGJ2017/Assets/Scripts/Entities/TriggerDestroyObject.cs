5283c4c8ba1a47794048807f76fbed49540c3955
DevRaptor <krzysztoftaperek@gmail.com>
2017-02-18 14:24:31 +0100

    Added TriggerDestroyObject

---
+++ WFSGJ2017/Assets/Scripts/Entities/TriggerDestroyObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDestroyObject : MonoBehaviour {
+
+	#region variables
+
+	[SerializeField]
+	GameObject objectToDestroy;
+
+	#endregion
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			Destroy(objectToDestroy);
+		}
+	}
+}
