d76acce67ea492dfb7e290cb90a14dca6b144db4
Dante <daniellewinski95@gmail.com>
2017-02-18 02:51:51 +0100

    Tried to fix jump collisions.

--- WFSGJ2017/Assets/Scripts/Entities/Player.cs
+++ WFSGJ2017/Assets/Scripts/Entities/Player.cs
@@ -4,53 +4,63 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    #region variables
-    bool isJumping;
+	#region variables
+	bool isJumping;
 
-    [SerializeField]
-    float jumpForceY;
+	[SerializeField]
+	float jumpForceY;
 
-    #endregion
+	#endregion
 
-    #region properties
+	#region properties
 
-    #endregion
+	#endregion
 
+	#region methods
 
-    #region methods
-
-    void Start ()
-    {
-        isJumping = true;
-        jumpForceY = 300;
+	void Start ()
+	{
+		isJumping = true;
+		jumpForceY = 300;
 	}
 
 	void Update ()
-    {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            Jump();
-        }
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Jump();
+		}
 	}
 
-    void Jump()
-    {
-        if (isJumping)
-            return;
+	void Jump()
+	{
+		if (isJumping)
+			return;
 
-        isJumping = true;
-        GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForceY));
-    }
+		isJumping = true;
+		GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForceY));
+	}
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
+	void OnCollisionEnter2D(Collision2D collision)
+	{
 		GameObject collider = collision.collider.gameObject;
-        if (collider.tag == "obstacle" 
-			&& (collider.transform.position.y - collider.transform.localScale.y) < (transform.position.y - transform.localScale.y))
-        {
-            isJumping = false;
+		if (collider.tag == "obstacle" )
+		{
+			//Debug.Log((collider.transform.position.y + collider.transform.localScale.y / 2));
+			//Debug.Log((transform.position.y - transform.localScale.y / 2));
+			if (collider.transform.position.y < transform.position.y)
+			{
+				//if((transform.position.x - transform.localScale.x) > transform.position )
+				Debug.Log("Jump reset");
+				isJumping = false;
+			}
+			else
+			{
+				//Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+				//rigidbody.velocity = new Vector2(-rigidbody.velocity.x, rigidbody.velocity.y);
+			}
 		}
-    }
+	}
 
-    #endregion methods
+	#endregion methods
 }
