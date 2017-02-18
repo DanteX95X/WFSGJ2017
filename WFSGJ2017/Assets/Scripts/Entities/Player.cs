1830821548cb8e6a59efb4f994e46fd7fc1c531a
Dante <daniellewinski95@gmail.com>
2017-02-18 01:54:34 +0100

    Jumping mechaninic, Obstacle prefab.

--- WFSGJ2017/Assets/Scripts/Entities/Player.cs
+++ WFSGJ2017/Assets/Scripts/Entities/Player.cs
@@ -4,13 +4,47 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    #region variables
 
-	void Start ()
+    bool isJumping;
+
+    #endregion
+
+    #region properties
+
+    #endregion
+
+
+    #region methods
+
+    void Start ()
     {
-        Debug.Log("Player created");	
+        Debug.Log("Player created");
+        isJumping = true;
 	}
 
 	void Update ()
     {
+        if(Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            Jump();
+        }
 	}
+
+    void Jump()
+    {
+        isJumping = true;
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1000));
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.tag == "obstacle")
+        {
+            Debug.Log("On ground");
+            isJumping = false;
+        }
+    }
+
+    #endregion methods
 }
